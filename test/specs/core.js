f8bfb6623f23b1807be8559e47d9620bdb70fd7f
André Cruz <andremiguelcruz@msn.com>
2013-06-19 18:18:14 +0100

    CS.

--- test/specs/core.js
+++ test/specs/core.js
@@ -5,16 +5,11 @@ define([
     'use strict';
 
     describe('spoon', function () {
-
         it('should be an object exposing the core', function () {
-
             expect(spoon).to.be.an('object');
             expect(spoon.Controller).to.be.a('function');
             expect(spoon.View).to.be.a('function');
             expect(spoon.BaseView).to.be.a('function');
-
         });
-
     });
-
 });
